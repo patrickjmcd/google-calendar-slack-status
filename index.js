6ce79386172125d266f10edc0b375db892f66711
patrickjmcd <patrickjmcd@gmail.com>
2018-09-27 10:34:57 -0500

    Expire status when event ends

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
-const slack = require('slack');
-const moment = require('moment');
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const slack = require("slack");
+const moment = require("moment");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const router = express.Router();
 
-app.post('/', (req, res, next) => {
-  // check for secret token
-  if (!req.body.token || req.body.token !== process.env.SECRET_TOKEN) next();
-  // grab status and clean it up
-  let status = req.body.title;
-  const dndToken = ' [DND]';
-  // parse event start/stop time
-  const dateFormat = 'MMM D, YYYY [at] hh:mmA';
-  const start = moment(req.body.start, dateFormat);
-  const end = moment(req.body.end, dateFormat);
-  // check for DND
-  if (status.includes(dndToken)) {
-    slack.dnd.setSnooze({
-      token: process.env.SLACK_TOKEN,
-      num_minutes: end.diff(start, 'minutes')
+app.post("/", (req, res, next) => {
+    // check for secret token
+    if (!req.body.token || req.body.token !== process.env.SECRET_TOKEN) next();
+    // grab status and clean it up
+    let status = req.body.title;
+    const dndToken = " [DND]";
+    // parse event start/stop time
+    const dateFormat = "MMM D, YYYY [at] hh:mmA";
+    const start = moment(req.body.start, dateFormat);
+    const end = moment(req.body.end, dateFormat);
+    const endEpoch = moment(req.body.end, dateFormat).unix();
+    // check for DND
+    if (status.includes(dndToken)) {
+        slack.dnd.setSnooze({
+            token: process.env.SLACK_TOKEN,
+            num_minutes: end.diff(start, "minutes")
+        });
+        status = status.replace(dndToken, "");
+    }
+    // set status
+    slack.users.profile.set({
+        token: process.env.SLACK_TOKEN,
+        profile: JSON.stringify({
+            status_text: `${status} from ${start.format(
+                "h:mm"
+            )} to ${end.format("h:mm a")} ${process.env.TIME_ZONE}`,
+            status_emoji: ":spiral_calendar_pad:",
+            status_expiration: endEpoch
+        })
     });
-    status = status.replace(dndToken, '');
-  }
-  // set status
-  slack.users.profile.set({
-    token: process.env.SLACK_TOKEN,
-    profile: JSON.stringify({
-      "status_text": `${status} from ${start.format('h:mm')} to ${end.format('h:mm a')} ${process.env.TIME_ZONE}`
-    })
-  });
-  res.status(200);
-  res.send('🤘');
+    res.status(200);
+    res.send("🤘");
 });
 
-app.get('/', (req, res, next) => {
-  // welcome message
-  res.send(`
+app.get("/", (req, res, next) => {
+    // welcome message
+    res.send(`
     <!DOCTYPE html>
     <html>
       <head>
@@ -74,8 +79,8 @@ app.get('/', (req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  res.status(404);
-  res.send('Not found');
+    res.status(404);
+    res.send("Not found");
 });
 
 app.listen(port);
